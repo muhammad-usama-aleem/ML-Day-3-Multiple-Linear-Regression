add3259116146b575aed5155c4ba048e16e810dd
Muhammmad Usama Aleem <56190389+muhammad-usama-aleem@users.noreply.github.com>
2021-08-06 15:00:50 +0500

    Add files via upload

--- multiple-linear-regression.py
+++ multiple-linear-regression.py
@@ -18,7 +18,7 @@ from sklearn.compose import ColumnTransformer
 labelencoder_x = LabelEncoder()
 x[:, 3] = labelencoder_x.fit_transform(x[:, 3])
 onehotencoder = ColumnTransformer(
-    [('one_hot_encoder', OneHotEncoder(categories='auto'), [0])],
+    [('one_hot_encoder', OneHotEncoder(categories='auto'), [3])],
     remainder='passthrough'
 )
 x = onehotencoder.fit_transform(x)
@@ -29,5 +29,24 @@ x = x[:, 1:]
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 1/3, random_state = 0)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
+
+# fitting simple linear regression to the training set
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+# predicting the test results
+y_predict = regressor.predict(x_test)
+
+
+# building optimal model using backward elimination method
+import statsmodels.api as sm
+one = np.ones((50, 1), dtype=int)
+x = np.append(one, x, axis=1)
+
+x_opt =  np.array(x[:, [0, 1, 2, 3, 4, 5]], dtype=float)
+regressor_ols = sm.OLS(endog=y, exog=x_opt).fit()
+regressor_ols.summary()
